3675cf39b9b7dd8bfdb2c60019a287066eb694d6
Agia <agia@pm.me>
2023-01-27 19:56:43 +0000

    Created a function for current forecast data retrieval and a function to render data to the page, also added a no input clause on the event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@
 let searchButton = document.querySelector("#button-search");
 
 // ** VARIABLES (Data) ** //
+// Assign the #search input value to a variable
+let cityInput = document.querySelector("#search").value;
+
 // Variables to store the return API values for their respective names
 let currentTemp;
 let currentHumidity;
@@ -10,7 +13,7 @@ let currentWind;
 let iconCode;
 let description;
 let iconURL;
-
+// Variables to store the future daily forecasts data
 let futureDayTemp;
 let futureDayHumidity;
 let futureDayWind;
@@ -29,8 +32,13 @@ searchButton.addEventListener("click", function (event) {
     // Prevents default action
     event.preventDefault();
 
-    // Calls the function getCityGeodes function
-    getCityGeocodes();
+    if (cityInput !== "") {
+        // Calls the function getCityGeodes function
+        getCityGeocodes();
+    } else {
+        return;
+    }
+
 })
 
 
@@ -39,10 +47,10 @@ searchButton.addEventListener("click", function (event) {
 
 // Function to convert the string input from user (city) to it's geocode equivalent, using the OpenWeather API
 function getCityGeocodes () {
-    // Assign the #search input value to a variable
-    let cityName = document.querySelector("#search").value;
-    // Assign the API URL, with the dynamic value of cityName, to a variable
-    let geoURL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=c1055e335571b836bde1ca735096c6bc`;
+    // // Assign the #search input value to a variable
+    // let cityInput = document.querySelector("#search").value;
+    // Assign the API URL, with the dynamic value of cityInput, to a variable
+    let geoURL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=5&appid=c1055e335571b836bde1ca735096c6bc`;
 
     // Create a fetch request to the geocoding API, passing in geoURL, and returning (and storing) the longitude and latitude of the response
     fetch(geoURL)
@@ -66,17 +74,16 @@ function getCityGeocodes () {
       })
 }
 
+
+
 // Function that takes a correctly formatted URL and assigns parts of the response to variables
 function getCurrentWeather(cityURL) {
     fetch(cityURL)
     .then (response => response.json())
     .then (function (data) {
         
-        // console.log(cityURL);
-        // console.log(data);
-
         // Stores the description of weather condition
-        description = data.weather[0].description;
+        description = (data.weather[0].description).toUpperCase();
         // Stores the icon code of the weather condition
         iconCode = data.weather[0].icon;
         // Stores the current temperature, and rounds it to the nearest integer
@@ -89,10 +96,15 @@ function getCurrentWeather(cityURL) {
         cityTitle = data.name;
         // Stores the URL of the icon image corresponding to the retrieved iconCode, previously stored
         iconURL = `https://openweathermap.org/img/wn/${iconCode}@2x.png`
+
+        renderCurrentWeatherInfo();
   
     })
 }
 
+
+
+// Function to retrive and store the data for the 4 day future forecast (i.e. not today)
 function getNextFourDaysForecast (fiveDayURL) {
     fetch(fiveDayURL)
     .then (response => response.json())
@@ -107,23 +119,68 @@ function getNextFourDaysForecast (fiveDayURL) {
         for (let i = 0; i < weatherData.length; i++) {
             const index = weatherData[i];
 
-            futureDayTemp = Math.round(index.main.temp);
+            // Variable storing their respective data, for future forecast days (4 not including current)
             futureDayHumidity = index.main.humidity;
+            // As above, but output rounded to the nearest integer
+            futureDayTemp = Math.round(index.main.temp);
             futureDayWind = index.wind.speed;
+            // Stores the relevant code for use in building the correct URL to retrieve the icon
             futureDayIconCode = index.weather[0].icon;
-            futureDayDescription = index.weather[0].description;
+            // Also uppercases the all lowercase data received
+            futureDayDescription = (index.weather[0].description).toUpperCase();
+            // With the addition of the dynamic variable, it stores the URL for matching icon to weather conditions
             futureDayIconURL = `https://openweathermap.org/img/wn/${futureDayIconCode}@2x.png`;
 
-            console.log(futureDayDescription);
-            console.log(futureDayHumidity);
-            console.log(futureDayIconCode);
-            console.log(futureDayIconURL);
-            console.log(futureDayTemp);
-            console.log(futureDayWind);
+            // Calls the function to render the information stored above, on the page
+            renderFutureWeatherInfo();
         }
 
     })
 }
+
+
+
+// Renders the data for the future forecast (i.e. not current / todays) to the page
+function renderFutureWeatherInfo () {
+
+    let dailyCollection = document.getElementById("bottom");
+    
+
+    for (let i = 0; i < dailyCollection.children.length; i++) {
+        const index = dailyCollection.children[i];
+        console.log(index);
+
+        index.innerHTML = `<h4>${cityTitle}</h4>
+        
+            <p class="temp">${futureDayTemp}</p>
+            <p class="desc">${futureDayDescription}</p>
+            <img src=${futureDayIconURL} />
+            <p class="humidity">${futureDayHumidity}</p>
+            `;
+
+        // index.setAttribute("style", `backgroundImage: ${futureDayIconURL}`);
+
+        dailyCollection.appendChild(index);
+        
+    }
+}
+
+
+// Render the current / todays weather data to the page
+function renderCurrentWeatherInfo () {
+
+    let currentInfoElement = document.getElementById("current-weather");
+
+        currentInfoElement.innerHTML = `<h4 class="city-lg">${cityTitle}</h4>
+        
+            <p class="temp-lg">${currentTemp}</p>
+            <p class="desc-lg">${description}</p>
+            <img class="icon-lg" src=${iconURL} />
+            <p class="humidity-lg">${currentHumidity}</p>
+            `;
+}
+
+
 
 // ? API Reference
 // main.temp
@@ -135,5 +192,3 @@ function getNextFourDaysForecast (fiveDayURL) {
 // name (London)
 
 // 0 7 15 22 30
-
-// https://api.openweathermap.org/data/2.5/forecast?lat=57&lon=-2.15&unit=metric&appid=c1055e335571b836bde1ca735096c6bc
